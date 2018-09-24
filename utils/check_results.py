baf9c86d232232c50c6abdb80a6a431df5a359fe
ClaraBing <bingbin@cs.stanford.edu>
2018-09-24 08:53:53 -0700

    check loss/metric curves

---
+++ utils/check_results.py
@@ -0,0 +1,50 @@
+import os
+from glob import glob
+import torch
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def plot_lines(lines, labels, fout):
+  assert(len(lines) == len(labels))
+  fig = plt.figure()
+  ax = plt.subplot(111)
+  for (line, label) in zip(lines, labels):
+    plt.plot(range(len(line)), line, label=label)
+
+  # shrink current axis by 20%
+  box = ax.get_position()
+  ax.set_position([box.x0, box.y0, box.width*0.8, box.height])
+  # put legend to the right of the current axis
+  ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+  plt.savefig(fout)
+  plt.clf()
+
+def get_loss_metric(fckpts, label_removes=[]):
+  losses, metrics, labels = [], [], []
+  for fckpt in fckpts:
+    label = fckpt.split('/')[-1].split('.')[0]
+    for each in label_removes:
+      label = label.replace(each, '')
+    ckpt = torch.load(fckpt)
+    loss, metric = ckpt['loss_history'], ckpt['metric_history']
+    losses += loss,
+    metrics += metric,
+    labels += label,
+  return losses, metrics, labels
+
+def get_loss_metric_wrapper():
+  ckpt_root = '/vision2/u/bingbin/ORN/'
+
+  fckpts = glob(os.path.join(ckpt_root, '*heads*gcnObj*', '*best.pth'))
+  # fckpts = [glob(os.path.join(fckpt_dir, '*best.pth')) for fckpt_dir in fckpt_dirs]
+  label_removes = ['epic_', 'bt8_lr1e-04_wd1e-05_', '_oEmb128_vEmb128_nLayer2_nTopObjs10']
+
+  losses, metrics, labels = get_loss_metric(fckpts, label_removes)
+
+  plot_lines(losses, labels, fout=os.path.join(ckpt_root, 'result_losses.png'))  
+  plot_lines(metrics, labels, fout=os.path.join(ckpt_root, 'result_metrics.png'))
+
+
+if __name__ == '__main__':
+  get_loss_metric_wrapper()
