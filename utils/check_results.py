aec2b7eb2def579156410d84558370080e65f4de
ClaraBing <bingbin@cs.stanford.edu>
2018-10-17 12:15:37 -0700

    compare training status after 1 epoch

--- utils/check_results.py
+++ utils/check_results.py
@@ -5,6 +5,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+ckpt_root = '/vision2/u/bingbin/ORN/ckpt'
+
 def plot_lines(lines, labels, fout):
   assert(len(lines) == len(labels))
   fig = plt.figure()
@@ -34,8 +36,6 @@ def get_loss_metric(fckpts, label_removes=[]):
   return losses, metrics, labels
 
 def get_loss_metric_wrapper():
-  ckpt_root = '/vision2/u/bingbin/ORN/'
-
   fckpts = glob(os.path.join(ckpt_root, '*heads*gcnObj*', '*best.pth'))
   # fckpts = [glob(os.path.join(fckpt_dir, '*best.pth')) for fckpt_dir in fckpt_dirs]
   label_removes = ['epic_', 'bt8_lr1e-04_wd1e-05_', '_oEmb128_vEmb128_nLayer2_nTopObjs10']
@@ -47,12 +47,13 @@ def get_loss_metric_wrapper():
 
 
 def check_epic_loss_from_log():
-  ckpt_root = '/vision2/u/bingbin/ORN/'
+  # experiments = glob(os.path.join(ckpt_root, 'epic_headsobject_*'))
+  experiments = ['/vision2/u/bingbin/ORN/ckpt/epic_headscontext_gcnObj0_gcnCtxt0_bt8_lr5e-04_wd1e-05_fromEpic']
   label_removes = ['epic_', 'bt8_lr1e-04_wd1e-05_', '_oEmb128_vEmb128_nLayer2_nTopObjs10']
 
   losses = []
   labels = []
-  for each in glob(os.path.join(ckpt_root, 'epic_headsobject_*')):
+  for each in experiments:
     flog = glob(os.path.join(each, 'train*.log'))
     if not flog:
       continue
@@ -67,6 +68,34 @@ def check_epic_loss_from_log():
       print('{}: {}'.format(label, loss))
   plot_lines(losses, labels, fout=os.path.join(ckpt_root, 'epic_loss_from_log.png'))
 
+
+def compare_first_epoch():
+  experiments = glob(os.path.join(ckpt_root, 'epic*05_*'))
+  label_removes = ['epic_', 'lr1e-04_wd1e-05_', '_oEmb128_vEmb128_nLayer2_nTopObjs10']
+
+  losses = []
+  for exp in experiments:
+    flog = glob(os.path.join(exp, 'train*.log'))
+    if not flog:
+      continue
+    flog = flog[0]
+    with open(flog, 'r') as fin:
+      try:
+        loss = float(fin.readline().strip().split(' ')[1].split('=')[1][:-1])
+        label = flog.split('/')[-2].split('.')[0]
+        for each in label_removes:
+          label = label.replace(each, '')
+        losses += (loss, label),
+      except Exception as e:
+        print('Error reading log:', flog)
+        print(e)
+        print()
+
+  losses = sorted(losses, key=lambda x:x[0])
+  for (loss, label) in losses:
+    print('{:.3f}: {}'.format(loss, label))
+
 if __name__ == '__main__':
   # get_loss_metric_wrapper()
-  check_epic_loss_from_log()
+  # check_epic_loss_from_log()
+  compare_first_epoch()
